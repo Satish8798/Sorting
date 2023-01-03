67ef4fe0973bdc014a63a4d90e36949ce3701874
Satish <105753235+Satish8798@users.noreply.github.com>
2023-01-03 18:12:08 +0000

    add selection sort

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function bubbleSort(list) {
   let swapped = false
   for (let i = 0; i < listLength; i++) {
     console.log("hi")
-    for (let j = 0; j < listLength; j++) {
+    for (let j = 0; j < listLength-1; j++) {
       if (list[j] > list[j + 1]) {
         let temp = list[j];
         list[j] = list[j + 1];
@@ -22,4 +22,32 @@ function bubbleSort(list) {
   console.log(list);
 }
 
-bubbleSort([1, 2, 4, 2, 5, 6])
+bubbleSort([1, 3,2,4,5]) 
+
+//Selection sort
+
+function selectionSort(list){
+  let length = list.length;
+  let swapped = false;
+  for(let i=0;i<length;i++){
+      let smallest = list[i];
+      let smallestIndex=i;
+    console.log('hi')
+    for(j=i+1;j<length;j++){
+      if(smallest>list[j]){
+        smallest = list[j]
+        smallestIndex=j
+      }
+      if(j===length-1){
+        let temp = list[i];
+        list[i]= list[smallestIndex];
+        list[smallestIndex] = temp;
+        swapped = true;
+      }
+    }
+    if(!swapped) break;
+  }
+  console.log(list)
+}
+
+selectionSort([1,3,2,4,5])
