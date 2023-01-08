d5233aae4d13d3578acd15a1c083ffe3f8995c65
Satish <105753235+Satish8798@users.noreply.github.com>
2023-01-08 17:40:43 +0000

    add merge sort

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function bubbleSort(list) {
   let listLength = list.length;
   let swapped = false
   for (let i = 0; i < listLength; i++) {
-    for (let j = 0; j < listLength-1; j++) {
+    for (let j = 0; j < listLength - 1; j++) {
       if (list[j] > list[j + 1]) {
         let temp = list[j];
         list[j] = list[j + 1];
@@ -22,24 +22,24 @@ function bubbleSort(list) {
   console.log(list);
 }
 
-bubbleSort([1, 2,3,4,5]) 
+bubbleSort([1, 2, 3, 4, 5])
 
 //Selection sort
 
-function selectionSort(list){
+function selectionSort(list) {
   console.log('selection sort')
   let length = list.length;
-  for(let i=0;i<length;i++){
-      let smallest = list[i];
-      let smallestIndex=i;
-    for(j=i+1;j<length;j++){
-      if(smallest>list[j]){
+  for (let i = 0; i < length; i++) {
+    let smallest = list[i];
+    let smallestIndex = i;
+    for (j = i + 1; j < length; j++) {
+      if (smallest > list[j]) {
         smallest = list[j]
-        smallestIndex=j
+        smallestIndex = j
       }
-      if(j===length-1){
+      if (j === length - 1) {
         let temp = list[i];
-        list[i]= list[smallestIndex];
+        list[i] = list[smallestIndex];
         list[smallestIndex] = temp;
       }
     }
@@ -47,42 +47,42 @@ function selectionSort(list){
   console.log(list)
 }
 
-selectionSort([1,2,3,4,5]);
+selectionSort([1, 2, 3, 4, 5]);
 
 //Insertion sort
 
-function insertionSort(list){
+function insertionSort(list) {
   console.log('insertion sort')
   let length = list.length;
   let swapped = false;
-  for(let i=0;i<length-1;i++){
-     if(list[i]>list[i+1]){
-       for(let j=i+1;j>=0;j--){
-         if(list[j]<list[j-1]){
-           let temp = list[j];
-           list[j]= list[j-1];
-           list[j-1] = temp;
-           swapped = true;
-         }
-       }
-       if(!swapped ) break;
-     }
+  for (let i = 0; i < length - 1; i++) {
+    if (list[i] > list[i + 1]) {
+      for (let j = i + 1; j >= 0; j--) {
+        if (list[j] < list[j - 1]) {
+          let temp = list[j];
+          list[j] = list[j - 1];
+          list[j - 1] = temp;
+          swapped = true;
+        }
+      }
+      if (!swapped) break;
+    }
   }
   console.log(list)
 }
-insertionSort([1,5,4,3,2]);
+insertionSort([1, 2, 4, 3, 2]);
 
-//merge sort 
+//Merge sort 
 
 const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 
-function mergeSort (array) {
+function mergeSort(array) {
   if (array.length === 1) {
     return array
   }
-  let mid = parseInt(array.length/2)
+  let mid = parseInt(array.length / 2)
   // Split Array into right and left
-  let left = array.slice(0,mid);
+  let left = array.slice(0, mid);
   let right = array.slice(mid);
   return merge(
     mergeSort(left),
@@ -90,10 +90,10 @@ function mergeSort (array) {
   )
 }
 
-function merge(left, right){
-  if(right===undefined) return left
-  return right.concat(left).sort((a,b)=>{
-    return a-b
+function merge(left, right) {
+  if (right === undefined) return left
+  return right.concat(left).sort((a, b) => {
+    return a - b
   });
 }
 
